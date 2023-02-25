7dceb8b60fce151d004ba4751df1649c57c36352
azne <a_kurbanov@hotmail.com>
2023-02-25 15:26:08 +0100

    update

--- main.js
+++ main.js
@@ -1,46 +1,34 @@
-let value;
 let netSalary;
 let taxData;
-const hours = document.getElementById("hours").value;
-const salaryHour = document.getElementById("salaryHour").value;
-const salaryMonthly = document.getElementById("salaryMonthly").value;
+const tableNum = document.getElementById("tableNum");
+const salaryInput = document.getElementById("salary");
 
-// async function getTax() {
-//     // const api_url = 'https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=34&inkomst%20t.o.m.=25000&%C3%A5r=2023&_limit=100&_offset=0';
-//     // const api_url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?inkomst%20t.o.m.=${salary}&%C3%A5r=2023&_limit=100&_offset=0`;
-//     const response = await fetch(api_url);
-//     const data = await response.json();
-// 	 taxData = data.results[0]["kolumn 1"];
-//     console.log(data);
+// async function getTax(tableNum, salaryInput) {
+//   const url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=${tableNum}&inkomst%20t.o.m.=${salaryInput}&%C3%A5r=2023&_limit=100&_offset=0`;
 
+//   const response = await fetch(url);
+//   const data = await response.json();
+
+//   if (data.resultCount <= 0) {
+//     console.log("Prova skriv annan siffra!");
+//   } else {
+//     taxData = data.results[0]["kolumn 1"];
+//   }
 // }
 
- async function getTax(table, salary) {
-  const url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=${table}&inkomst%20t.o.m.=${salary}&%C3%A5r=2023&_limit=100&_offset=0`;
-
-  const response = await fetch(url);
-  const data = await response.json();
-}
-
 async function calculateSalary() {
+  const salary = salaryInput.value;
+  const selectedTableNum = tableNum.value;
+	var taxData = 4000;
+//   await getTax(selectedTableNum, salary);
 
-
-  var grossSalary = hours * salaryHour;
-
-  var city = document.getElementById("city").value;
-  switch (city) {
-    case "sundsvall":
-      await getTax(); // call getTax() to retrieve the taxData value
-      break;
-  }
-
-  if (grossSalary <= 0) {
-    netSalary = salaryMonthly - taxData;
-  } else {
-    netSalary = grossSalary - taxData;
-  }
-
-  document.getElementById(
-    "salary"
-  ).innerHTML = `Your net salary is $${netSalary.toFixed(2)}.`;
+  netSalary = salary - taxData;
+  console.log((netSalary = salary - taxData));
+  displayTaxData();
 }
+
+function displayTaxData() {
+	const taxDataElement = document.getElementById("result");
+	taxDataElement.innerText = `Lön efter skatt ${netSalary}.`;
+
+  }

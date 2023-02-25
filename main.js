a5d0b197ff773dcec95428e91640f1617f8e2622
azne <a_kurbanov@hotmail.com>
2023-02-26 00:26:30 +0100

    update

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 let netSalary;
-let taxData;
+let taxData = 0;
 const tableNum = document.getElementById("tableNum");
-const salaryInput = document.getElementById("salary");
+const salaryInput = document.getElementById("userSalary").value;
 const taxDataElement = document.getElementById("result");
 let salaryTo;
 let salaryFrom;
+let changedValue;
 
-async function getTax(tableNum, salaryTo, salaryFrom) {
+
+
+
+
+
+
+async function getTax(tableNum, changedValue) {
   // const url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=${tableNum}&inkomst%20t.o.m.=${salaryInput}&%C3%A5r=2023&_limit=100&_offset=0`;
   //const url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=${tableNum}&%C3%A5r=2023&inkomst%20fr.o.m.=${salaryInput}&_limit=100&_offset=0`;
   //const url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=${tableNum}&inkomst%20t.o.m.=${salaryInput}&%C3%A5r=2023&_limit=100&_offset=0`;
   //const url = `https://skatteverket.entryscape.net/rowstore/dataset/c67b320b-ffee-4876-b073-dd9236cd2a99?kommun=${tableNum}&%C3%A5r=2023&_limit=100&_offset=0`;
-  
-  console.log(salaryInput);
-  const url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=${tableNum}&inkomst%20t.o.m.=${salaryTo}&%C3%A5r=2023&inkomst%20fr.o.m.=${salaryFrom}&_limit=100&_offset=0`;
 
-  const response = await fetch(url);
-  const data = await response.json();
+  //const url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=${tableNum}&inkomst%20t.o.m.=${salaryTo}&%C3%A5r=2023&inkomst%20fr.o.m.=${salaryFrom}&_limit=100&_offset=0`;
+  //const url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=${tableNum}&%C3%A5r=2023&inkomst%20fr.o.m.=${salaryInput}&_limit=100&_offset=0`;
+  const url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=${tableNum}&inkomst%20t.o.m.=${changedValue}&%C3%A5r=2023&_limit=100&_offset=0`;
+
+  // if (salaryInput.toString().slice(2) !== "00") {
+  //    changedValue = salaryInput.toString().slice(0, -2) + "00";
+ 
+  //  console.log( changedValue);
 
 
-  taxData = await data.results[0]["kolumn 1"];
-  console.log(taxData);
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data.results);
+
+
+    taxData = await data.results[0]['kolumn 1'];
+    console.log( taxData);
+  // }
+
+ 
 }
 
 async function calculateSalary() {

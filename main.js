737b4d04d6b3f689998ae9e2ef17ebad554791b2
azne <a_kurbanov@hotmail.com>
2023-03-01 12:48:55 +0100

    update

--- main.js
+++ main.js
@@ -1,49 +1,65 @@
 let netSalary;
-let taxData = 0;
-let salaryTo;
-let salaryFrom;
+let taxData;
+let taxData1;
 let changedValue;
+let minNum;
+let maxNum;
 const tableNum = document.getElementById("tableNum");
 const salaryInput = document.getElementById("userSalary");
 const taxDataElement = document.getElementById("result");
 const submitButton = document.getElementById("submitSalary");
+let anum;
+
+async function testTax(tableNum, minNum, maxNum) {
+  const url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=${tableNum}&%C3%A5r=2023`;
+  const response = await fetch(url);
+  const data1 = await response.json();
+
+  console.log(data1);
+  console.log(minNum);
+  console.log(maxNum);
+
+  taxData1  = await data1.results[0]["kolumn 1"];
+  console.log(taxData1);
+
+  return { data1, minNum, maxNum };
+}
 
 async function getTax(tableNum, changedValue) {
   const url = `https://skatteverket.entryscape.net/rowstore/dataset/88320397-5c32-4c16-ae79-d36d95b17b95?tabellnr=${tableNum}&inkomst%20t.o.m.=${changedValue}&%C3%A5r=2023&_limit=100&_offset=0`;
+
   const response = await fetch(url);
   const data = await response.json();
 
-  taxData = await data.results[0]["kolumn 1"];
-  console.log(taxData);
+  if (data.resultCount == 0) {
+    changedValue += 100;
+    return getTax(tableNum, changedValue);
+  }
 
-  return data
+  return { data, changedValue };
 }
 
+// submitButton.addEventListener("click", async () => {
+//   if (salaryInput.value.slice(2) != "00") {
+//     changedValue = parseInt(salaryInput.value);
+//     changedValue = changedValue - (changedValue % 100);
+//   }
+
+//   let { data } = await getTax(tableNum.value, changedValue);
+//   taxData = await data.results[0]["kolumn 1"];
+//   displayTaxData(changedValue, netSalary, taxData);
+// });
+
 submitButton.addEventListener("click", async () => {
-  if (salaryInput.value.slice(2) != "00") {
-    changedValue = parseInt(salaryInput.value);
 
-    changedValue = changedValue - (changedValue % 100);
-    salaryInput.value = changedValue;
-  }
-  const salary = changedValue;
-  const selectedTableNum = tableNum.value;
 
-  const data = await getTax(selectedTableNum, salary);
+  let { data1 } = await testTax(tableNum.value, minNum, maxNum);
 
-  console.log(data);
-
-  if (data) {
-    netSalary = salary - taxData;
-    displayTaxData(salary, netSalary);
-  } else {
-    console.log("No data found!");
-  }
-
-  displayTaxData(salary, netSalary);
+  console.log(minNum);
+  console.log(maxNum);
 });
 
-function displayTaxData(salary, netSalary) {
+function displayTaxData(salary, netSalary, taxData) {
   netSalary = salary - taxData;
   const result = document.getElementById("result");
   const sum = document.getElementById("sum");

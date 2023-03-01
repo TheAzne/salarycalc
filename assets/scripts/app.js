021c8aff7ebf709f40cd19f89b262acf39c18e9d
AlexRasch <46262688+AlexRasch@users.noreply.github.com>
2023-03-01 16:37:01 +0100

    add fn to reset table

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,7 @@
 let elemPrint;
 
 let elemTable;
-let elemName,elemSalary, elemTaxe;
+let elemName, elemSalary, elemTaxe;
 let elemTotalExpense;
 
 /* Vars */
@@ -18,27 +18,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
     elemTable = document.getElementById('tblSalary');
     elemTotalExpense = document.getElementById('totalExpense');
 
+    // Events
     document.getElementById('btnPrint').addEventListener('click', fnPrint);
     document.getElementById('btnAdd').addEventListener('click', fnAdd);
+    document.getElementById('btnTableReset').addEventListener('click', fnTableReset);
 
 });
+/* Dom Change */
 
-function fnAdd(e){
+
+function fnAdd(e) {
     e.preventDefault();
     // Get value from form
     elemName = document.getElementById('inputName').value;
     elemSalary = document.getElementById('inputSalary').value;
     elemTaxe = document.getElementById('inputTaxe').value;
-    console.log('Adding:' + elemName, 'Salary:' + elemSalary, 'Taxe:' + elemTaxe );
+    console.log('Adding:' + elemName, 'Salary:' + elemSalary, 'Taxe:' + elemTaxe);
 
     // Validate input and sanitize
-   
-    
+
+
     // Process input / data
-    let iTaxe = CalcTaxe(elemSalary, elemTaxe);
-    
-    
-    
+    let iTaxe = fnCalcTaxe(elemSalary, elemTaxe);
+
+
     // Add to table
     fnTableAdd(elemName, elemSalary, iTaxe);
 
@@ -57,8 +60,8 @@ function fnTableAdd(strName, iSalary, iTaxe) {
     row.insertCell(4).innerHTML = iTaxe;
 
     // Edit / Delete entry button
-    row.insertCell(5).innerHTML = fnTableAddBtn('delete',iTableCurrent);
-    row.insertCell(6).innerHTML = fnTableAddBtn('edit',iTableCurrent);
+    row.insertCell(5).innerHTML = fnTableAddBtn('delete', iTableCurrent);
+    row.insertCell(6).innerHTML = fnTableAddBtn('edit', iTableCurrent);
 
 
     // Increase off set
@@ -67,7 +70,13 @@ function fnTableAdd(strName, iSalary, iTaxe) {
 }
 
 // Remvoes all entry from table
-function fnResetTable(){
+function fnTableReset() {
+    console.log('Table reset');
+    var tableHeaderRowCount = 1;
+    var rowCount = elemTable.rows.length;
+    for (var i = tableHeaderRowCount; i < rowCount; i++) {
+        elemTable.deleteRow(tableHeaderRowCount);
+    }
 
 }
 
@@ -76,19 +85,19 @@ function fnResetTable(){
 // Delete Entry
 
 // Add/Remove btn for table entry
-function fnTableAddBtn(fnBtnType, iEntryId){
-    if(fnBtnType == 'delete')
-    return '<button class="btn btn-outline-danger"'+ fnTableBtnGetDataUrl(iEntryId) +'><i class="bi bi-trash"></i></button>';
-    if(fnBtnType == 'edit')
-    return '<button class="btn btn-outline-info"'+ fnTableBtnGetDataUrl(iEntryId) +'><i class="bi bi-pen"></i></button>'
+function fnTableAddBtn(fnBtnType, iEntryId) {
+    if (fnBtnType == 'delete')
+        return '<button class="btn btn-outline-danger"' + fnTableBtnGetDataUrl(iEntryId) + '><i class="bi bi-trash"></i></button>';
+    if (fnBtnType == 'edit')
+        return '<button class="btn btn-outline-info"' + fnTableBtnGetDataUrl(iEntryId) + '><i class="bi bi-pen"></i></button>'
 }
 
 // Naming D:
-function fnTableBtnGetDataUrl(iEntryId){
-    return 'data-url="'+ iEntryId +'"';
+function fnTableBtnGetDataUrl(iEntryId) {
+    return 'data-url="' + iEntryId + '"';
 }
 
 // Total cost
-function fnTotalExpense(strInput){
-    elemTotalExpense.textContent = strInput; 
+function fnTotalExpense(strInput) {
+    elemTotalExpense.textContent = strInput;
 }
--- assets/scripts/taxe.js
+++ assets/scripts/taxe.js
@@ -26,3 +26,4 @@ async function fnCalcAmountOfTax(tableNum, minNum, maxNum) {
     }
 
 }
+
